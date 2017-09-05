a84d3f92f4b8380830bac1c0e9fa30702b2b4e3b
Art Calek <acalek@lulife.org>
2017-09-05 15:12:27 -0500

    some work in vs

---
+++ Database1/Database1/People.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[People]
+(
+	[EmployeeID] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [email] NVARCHAR(250) NULL
+)
---
+++ Database1/Database1/_1_ Initialize People.sql
@@ -0,0 +1,8 @@
+﻿insert
+	People
+values
+	('1','Art','Calek','ac@junk.tv'),
+	('2','Rocket','Squirel','rs@junk.tv'),
+	('3','Bullwinkle','Moose','bm@junk.tv'),
+	('4','Fred','Flintstone','ff@junk.tv'),
+	('5','Barney','Rubble','br@junk.tv')
