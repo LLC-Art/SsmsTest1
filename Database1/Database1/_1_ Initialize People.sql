ca4e468d5be9a2f8338d40f6340d686aedcf2b2a
Art Calek <acalek@lulife.org>
2017-09-05 15:37:22 -0500

    Commit from desktop github

--- Database1/Database1/_1_ Initialize People.sql
+++ Database1/Database1/_1_ Initialize People.sql
@@ -1,12 +1,12 @@
-// Added in GitHub
+-- Added in GitHub
 truncate table People
-//
+--
 
 insert
 	People
 values
-	('1','Art','Calek','ac@junk.tv'),
-	('2','Rocket','Squirel','rs@junk.tv'),
-	('3','Bullwinkle','Moose','bm@junk.tv'),
-	('4','Fred','Flintstone','ff@junk.tv'),
-	('5','Barney','Rubble','br@junk.tv')
+	('1','Art','Calek','ac@cartoons.tv'),
+	('2','Rocket','Squirel','rs@cartoons.tv'),
+	('3','Bullwinkle','Moose','bm@cartoons.tv'),
+	('4','Fred','Flintstone','ff@cartoons.tv'),
+	('5','Barney','Rubble','br@cartoons.tv')
